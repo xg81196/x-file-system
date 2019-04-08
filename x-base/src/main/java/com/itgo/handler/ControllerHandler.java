5345bf39ba5eec07ae2da75fc085df05393e0375
xg81196 <81196xg>
2019-04-08 16:57:36 +0800

    去掉空代码

--- x-base/src/main/java/com/itgo/handler/ControllerHandler.java
+++ x-base/src/main/java/com/itgo/handler/ControllerHandler.java
@@ -4,11 +4,9 @@ import com.itgo.exception.ExceptionResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
-import javax.jws.WebResult;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
