8c41c3d72918680b2345abc9a210685bc7d52cda
xg81196 <81196xg>
2019-04-09 21:05:12 +0800

    切面

--- x-web/web-web/src/main/java/com/itgo/web/aspect/ControllerAspect.java
+++ x-web/web-web/src/main/java/com/itgo/web/aspect/ControllerAspect.java
@@ -1,7 +1,6 @@
 package com.itgo.web.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
